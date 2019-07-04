adc43efa0c240a9b33aa5046baa270ea54a8fba5
hafid <ach.hafid@gmail.com>
2019-07-04 23:05:37 +0700

    Release version 0.3.0

--- lib/src/main/java/io/github/achmadhafid/zpack/ktx/StringKtx.kt
+++ lib/src/main/java/io/github/achmadhafid/zpack/ktx/StringKtx.kt
@@ -2,7 +2,6 @@
 
 package io.github.achmadhafid.zpack.ktx
 
-import android.annotation.SuppressLint
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -20,16 +19,36 @@ fun String.toDate(format: String = "yyyy/MM/dd hh:mm"): Date? = try {
 
 val String.toCamelCase: String
     get() {
-        if (length == 0)
-            return this
-        val parts = split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        if (isEmpty()) return ""
+
         var camelCaseString = ""
-        for (part in parts) camelCaseString = camelCaseString + part.toTitleCase + " "
+        split(" ").forEach {
+            camelCaseString = when {
+                it.isEmpty() -> "$camelCaseString "
+                camelCaseString.trim().isEmpty() -> camelCaseString + it.toLowerCase(Locale.getDefault()) + " "
+                else -> camelCaseString + it.toTitleCase
+            }
+        }
         return camelCaseString
     }
 
 val String.toTitleCase: String
-    @SuppressLint("DefaultLocale")
-    get() = substring(0, 1).toUpperCase() + substring(1).toLowerCase()
+    get() {
+        if (isEmpty()) return ""
+
+        var titleCaseString = ""
+        split(" ").forEach {
+            titleCaseString = if (it.isEmpty()) {
+                "$titleCaseString "
+            } else {
+                titleCaseString + (
+                        it.substring(0, 1).toUpperCase(Locale.getDefault()) +
+                                it.substring(1).toLowerCase(Locale.getDefault()) +
+                                " "
+                        )
+            }
+        }
+        return titleCaseString
+    }
 
 //endregion
