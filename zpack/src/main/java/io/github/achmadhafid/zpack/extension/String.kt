47baedc1ca18a7702f9e793b616d171a3ff3c4a8
hafid <ach.hafid@gmail.com>
2020-06-27 17:42:18 +0700

    add String extensions

---
+++ zpack/src/main/java/io/github/achmadhafid/zpack/extension/String.kt
@@ -0,0 +1,17 @@
+package io.github.achmadhafid.zpack.extension
+
+//region Case
+
+inline val String.toCamelCase: String get() = (this as CharSequence).toCamelCase.toString()
+inline val String.toTitleCase: String get() = (this as CharSequence).toTitleCase.toString()
+
+//endregion
+//region Content
+
+infix fun String?.whenNullOrEmpty(placeholder: String) =
+    if (this?.isNotEmpty() == true) this else placeholder
+
+infix fun String?.whenNullOrBlank(placeholder: String) =
+    if (this?.isNotBlank() == true) this else placeholder
+
+//endregion
